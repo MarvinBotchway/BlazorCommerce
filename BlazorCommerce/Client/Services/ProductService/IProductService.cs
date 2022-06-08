33607a47f7b743121e202d5ca0dacdd1cada9399
Marvin Botchway <marvinbotchway@outlook.com>
2022-06-08 08:56:11 +0000

    Add Search Methods in ProductService On the CLient

--- BlazorCommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorCommerce/Client/Services/ProductService/IProductService.cs
@@ -4,7 +4,11 @@
     {
         event Action ProductsListChanged;
         public List<ProductModel> Products { get; set; }
+
+        string Message { get; set; }
         Task GetProductsAsync(string? categoryNameForUrl = null);
         Task<ServiceResponse<ProductModel>> GetSingleProductAsync(int? id);
+        Task SearchProductsAsync(string searchText);
+        Task<List<string>> GetProductSearchSuggestionsAsync(string searchText);
     }
 }
--- BlazorCommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorCommerce/Client/Services/ProductService/ProductService.cs
@@ -10,7 +10,8 @@ namespace BlazorCommerce.Client.Services.ProductsService
             _http = http;
         }
         public List<ProductModel> Products { get; set; } = new List<ProductModel>();
-        
+        public string Message { get; set; } = "Loading Products...";
+
         public event Action ProductsListChanged;
 
         public async Task GetProductsAsync(string? categoryNameForUrl = null)
@@ -25,11 +26,32 @@ namespace BlazorCommerce.Client.Services.ProductsService
             ProductsListChanged.Invoke();
         }
 
+        public async Task<List<string>> GetProductSearchSuggestionsAsync(string searchText)
+        {
+            var response = await _http
+                .GetFromJsonAsync<ServiceResponse<List<string>>>($"/api/product/searchsuggestions/{searchText}");
+            return response.Data;
+        }
+
         public async Task<ServiceResponse<ProductModel>> GetSingleProductAsync(int? id)
         {
             var response = await _http.GetFromJsonAsync<ServiceResponse<ProductModel>>($"/api/product/{id}");
             if (response != null && response.Data != null) return (response);
             throw new Exception();
+        }
+
+        public async Task SearchProductsAsync(string searchText)
+        {
+            var response = await _http
+                .GetFromJsonAsync<ServiceResponse<List<ProductModel>>>($"/api/product/search/{searchText}");
+
+            if (response != null && response.Data != null)
+                Products = response.Data;
+            
+            if (Products.Count == 0)
+                Message = "No products found";
+            ProductsListChanged?.Invoke();
+        
         }
     }
 }
